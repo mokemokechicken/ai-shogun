74e452d7331a9cfe1995f8abc56e8492a70a249a
Ken Morishita <mokemokechicken@gmail.com>
2026-02-03 15:56:48 +0000

    commit

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -17,7 +17,8 @@ const formatTime = (iso: string) =>
     month: "2-digit",
     day: "2-digit",
     hour: "2-digit",
-    minute: "2-digit"
+    minute: "2-digit",
+    second: "2-digit"
   });
 
 const dedupeMessages = (items: ShogunMessage[]) => {
